747bdc6ef1a91179c199f785104c0417fffbccab
Alex Li <alexxuanqili@gmail.com>
2024-12-24 20:45:40 -0500

    fix audio player

--- src/AudioPlayer.jsx
+++ src/AudioPlayer.jsx
@@ -14,7 +14,11 @@ const AudioPlayer = ({ src }) => {
 		audioRef.current.pause(); // Pause the audio before changing the source
 		setIdx((idx + 1) % src.length);
 		audioRef.current.load(); // Load the new source
-		audioRef.current.play(); // Play the new source
+		const playAudio = () => {
+			audioRef.current.play();
+			audioRef.current.removeEventListener("canplaythrough", playAudio);
+		};
+		audioRef.current.addEventListener("canplaythrough", playAudio);
 	};
 
 	return <audio ref={audioRef} src={src[idx]} preload="auto" onEnded={handleEnded} />;
